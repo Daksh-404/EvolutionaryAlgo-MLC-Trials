ec0bcca6bf898f1712413452751d04ffea39a28f
Daksh <dakshguptajune@gmail.com>
2023-03-09 19:44:19 +0530

    completed the modifications to GA, added methods to construct conditional entropy matrix using label space

--- EvolutionaryAlgorithms/GeneticAlgorithm.py
+++ EvolutionaryAlgorithms/GeneticAlgorithm.py
@@ -1,86 +1,145 @@
 import random
 
 class GeneticAlgorithm:
-    def __init__(self) -> None:
+    def __init__(self, chromosome_size, conditional_entropy_matrix) -> None:
         print("GA Activated!")
+        self.chromosome_size = chromosome_size
+        self.conditional_entropy_matrix = conditional_entropy_matrix
+        self.gene_pool = [i + 1 for i in range(chromosome_size)]
 
-    def generate_individual(gene_pool):
+    def fitness_fn(self, chromosome):
+        # This fitness function calculates the upper triangular matrix of Conditional Entropy
+        # after it is rearranged according to the given chromosome / permutation
+
+        # Reaarangement
+        new_objective_fn = []
+        for idx1 in chromosome:
+            row_conditional_entropy = []
+            for idx2 in chromosome:
+                row_conditional_entropy.append(self.conditional_entropy_matrix[idx1 - 1][idx2 - 1])
+            new_objective_fn.append(row_conditional_entropy)
+
+        # Sum of upper triangular matrix
+        fitness_val = 0
+        for i in range(self.chromosome_size):
+            for j in range(self.chromosome_size):
+                if (i < j):
+                    fitness_val = fitness_val + new_objective_fn[i][j]
+
+        return fitness_val
+    
+    def generate_individual(self):
         # Generate a random permutation of the gene pool
-        individual = gene_pool.copy()
+        individual = self.gene_pool.copy()
         random.shuffle(individual)
         return individual
 
-    def selection(population, fitness_scores):
+    def tournament_selection(self, population, fitness_scores):
         # Select parents using tournament selection
         num_parents = len(population)
         parents = []
         for i in range(num_parents):
             competitors = random.sample(list(enumerate(population)), k=2)
+            print(competitors)
             winner = max(competitors, key=lambda x: fitness_scores[x[0]])
             parents.append(winner[1])
         return parents
 
-    def crossover(parent1, parent2):
-        # Crossover by selecting a random section of the parents and swapping the genes
-        start = random.randint(0, len(parent1) - 1)
-        end = random.randint(start + 1, len(parent1))
-        child = parent1[start:end] + [g for g in parent2 if g not in parent1[start:end]]
-        return child
+    def roulette_selection(self, population, fitness_scores):
+        num_parents = len(population)
+        parents = []
+        for i in range(num_parents):
+            prob = random.uniform(0, sum(fitness_scores))
+            for j, fitness_val in enumerate(fitness_scores):
+                if prob <= 0:
+                    break
+                prob -= fitness_val
+            parents.append(population[j])
+            print(j)
+        return parents
 
-    def mutate(individual, gene_pool, mutation_rate):
+    def crossover_helper(self, parent1, parent2, start, end):
+        child = []
+        for i in range(start):
+            if parent2[i] not in parent1[start : end]:
+                child.append(parent2[i])
+
+        child = child + parent1[start : end]
+        for i in range(start, self.chromosome_size):
+            if parent2[i] not in parent1[start : end]:
+                child.append(parent2[i])
+        
+        return child
+    
+    def ordered_crossover(self, population, parent1, parent2, crossover_rate):
+        # Ordered Crossover by selecting a random section of the parents
+        start = random.randint(0, self.chromosome_size - 1)
+        end = random.randint(start + 1, self.chromosome_size)
+        child1 = []
+        child2 = []
+        if random.random() < crossover_rate:
+            child1 = self.crossover_helper(parent1, parent2, start, end)
+            child2 = self.crossover_helper(parent2, parent1, start, end)
+        else:
+            child1 = random.sample(population, k = 1)[0]
+            child2 = random.sample(population, k = 1)[0]
+        return child1, child2
+        
+
+    def mutate(self, chromosome, mutation_rate):
         # Mutate by randomly swapping two genes with the given mutation rate
-        mutated_individual = individual.copy()
-        for i in range(len(mutated_individual)):
-            if random.random() < mutation_rate:
-                j = random.randint(0, len(mutated_individual) - 1)
-                mutated_individual[i], mutated_individual[j] = mutated_individual[j], mutated_individual[i]
+        mutated_individual = chromosome.copy()
+        if random.random() < mutation_rate:
+            i = random.randint(0, len(mutated_individual) - 1)
+            j = random.randint(0, len(mutated_individual) - 1)
+            mutated_individual[i], mutated_individual[j] = mutated_individual[j], mutated_individual[i]
         return mutated_individual
     
-    def reproduction(parents, population_size):
+    def reproduction(self, parents, population, population_size, crossover_rate):
         # Reproduce by randomly combining pairs of parents and performing crossover
+        temp_parents = parents.copy()
         offspring = []
         while len(offspring) < population_size:
-            father = random.choice(parents)
-            mother = random.choice(parents)
-            if father != mother:
-                child = crossover(father, mother)
-                offspring.append(child)
+            father, mother = random.sample(temp_parents, k = 2)
+            child1, child2 = self.ordered_crossover(population, father, mother, crossover_rate)
+            offspring.append(child1)
+            offspring.append(child2)
+            temp_parents.remove(father)
+            temp_parents.remove(mother)
+            print(len(offspring))
         return offspring
 
+    def refine_population(self, offspring, parents, elitism_rate, population_size):
+        prev_gen = parents.copy()
+        new_gen = offspring.copy()
+        prev_gen.sort(key = self.fitness_fn, reverse = True)
+        new_gen.sort(key = self.fitness_fn, reverse = True)
+
+        new_population = prev_gen[:elitism_rate] + new_gen[:population_size - elitism_rate]
+        return new_population
     
-    def genetic_algorithm(population_size, num_generations, fitness_fn, gene_pool, mutation_rate=0.01):
+    def genetic_algorithm(self, population_size, num_generations, mutation_rate = 0.01, crossover_rate = 0.9, elitism_rate = 5):
         # Initialize the population
-        population = [generate_individual(gene_pool) for i in range(population_size)]
+        population = [self.generate_individual() for i in range(population_size)]
         
-        # for i in range(num_generations):
-        #     # Evaluate the fitness of each individual
-        #     fitness_scores = [fitness_fn(individual) for individual in population]
+        for i in range(num_generations):
+            # Evaluate the fitness of each individual
+            fitness_scores = [self.fitness_fn(chromosome) for chromosome in population]
+            # Select the parents for the next generation
+            parents = self.roulette_selection(population, fitness_scores)
             
-        #     # Select the parents for the next generation
-        #     parents = selection(population, fitness_scores)
+            # Reproduce to create the next generation
+            offspring = self.reproduction(parents, population, population_size, crossover_rate)
             
-        #     # Reproduce to create the next generation
-        #     offspring = reproduction(parents, population_size)
+            # Mutate the offspring
+            mutated_offspring = [self.mutate(chromosome, mutation_rate) for chromosome in offspring]
             
-        #     # Mutate the offspring
-        #     mutated_offspring = [mutate(individual, gene_pool, mutation_rate) for individual in offspring]
+            # Generate the new population using elitism
+            population = self.refine_population(mutated_offspring, parents, elitism_rate, population_size)
             
-        #     # Replace the old population with the new generation
-        #     population = mutated_offspring
         
         # Return the fittest individual
-        return max(population, key=fitness_fn)
+        return max(population, key = self.fitness_fn)
 
 
 
-
-
-gene_pool = [1, 2, 3, 4, 5, 6, 7, 8]
-
-def fitness_fn(individual):
-    # This fitness function calculates the number of correctly positioned elements in the permutation
-    return sum([1 for i, j in zip(individual, gene_pool) if i == j])
-
-geneObj = GeneticAlgorithm()
-fittest_permutation = geneObj.genetic_algorithm(population_size = 50, num_generations = 100, fitness_fn = fitness_fn, gene_pool = gene_pool)
-print(fittest_permutation)
