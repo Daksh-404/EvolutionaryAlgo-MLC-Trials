380553c7e2081f022b859b346d91b3b94a687409
Arete Avyaktam <74955982+AreteAvyaktam@users.noreply.github.com>
2023-07-16 17:58:54 +0530

    Update run.py

--- run.py
+++ run.py
@@ -34,24 +34,33 @@ def test_parameters(
     for metric in metrics_list:
         info_table[metric] = []
     for i in range(iterations):
-        # change to differential evolution here
         geneticObj = GeneticAlgorithm(len(dset_classes.columns), ce_matrix)
-        #remove mutation rate and elitism rate
+        s = time.time()
         label_order = geneticObj.genetic_algorithm(
-            population_size=100,
+            population_size=200,
             num_generations=generation,
             mutation_rate=mutation_rate,
             crossover_rate=crossover_rate,
             elitism_rate=elitism,
         )
+
+        e = time.time()
+        tr = e - s
+        tp = 0
         # Getting results on the given approach for MLC
+        #print("#################################")
+        print(name)
+        #print("#################################")
         stacked_chaining = StackedChaining(
             dset_features, dset_classes, split, label_order, name
         )
+
         stacked_chaining.run()
         for metric in metrics_list:
             info_table[metric].append(stacked_chaining.metric_values[metric])
         info_table["iteration"].append(i + 1)
+        print("training time: ", round(tr + stacked_chaining.training_time, 3))
+        print("prediction time: ", round(stacked_chaining.prediction_time, 3))
     return info_table
 
 
@@ -93,7 +102,7 @@ def run(
         info_table[metric] = []
 
     # Generating the optimal permutation for label ordering
-    ce_matrix = construct_conditional_entropy_matrix(dset_classes.to_numpy())
+    ce_matrix = construct_mutual_information_matrix(dset_classes.to_numpy())
     for gen_index in tqdm(range(len(generations))):
         generation = generations[gen_index]
         for mutation_rate in mutation_rate_trial_values:
@@ -122,21 +131,17 @@ def run(
                     info_table["Elitism Rate"] += [elitism] * iterations
     return info_table
 
-
 def run_pipeline():
     print("PIPELINE INITIATED!")
-
-    os.chdir(path_to_dataset)
     for file in os.listdir():
         if file.count("testing") == 0:
             split = int(file.split("_")[1].split(".")[0])
             name = file.split("_")[0]
-            
             dset = pd.read_csv(os.path.join(path_to_dataset, file))
             generations = [100]
-            mutation_rate_trial_values = list(np.linspace(start=0.01, stop=0.01, num=1))
-            crossover_rate_trial_values = list(np.linspace(start=0.8, stop=0.8, num=1))
-            elitism_trial_values = [14]
+            mutation_rate_trial_values = list(np.linspace(start=0.03, stop=0.03, num=1))
+            crossover_rate_trial_values = list(np.linspace(start=0.9, stop=0.9, num=1))
+            elitism_trial_values = [8]
 
             # Extracting the class and the feature dataframes from the data
             start = 1
@@ -144,6 +149,11 @@ def run_pipeline():
                 start = start + 1
             end = split + 1
 
+            if name == "rcv":
+              start = 0
+              end = end - 1
+            #print(start, end)
+            dset = dset.sample(frac=1).reset_index(drop=True)
             dset_features = dset.iloc[:, start : end]
             dset_classes = dset.iloc[:, end :]
             info_table = run(
@@ -155,10 +165,10 @@ def run_pipeline():
                 elitism_trial_values=elitism_trial_values,
                 split=split,
                 name=name,
-                iterations=10
+                iterations=1
             )
             csv = pd.DataFrame(data=info_table)
-            csv.to_csv(f"{name}_test_results.csv")
+            csv.to_csv(f"{path_to_dataset}/{name}_test_results.csv")
     print("PIPELINE CLOSED!")
 
 
